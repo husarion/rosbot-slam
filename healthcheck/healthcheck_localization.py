ed5028d31a7d88d732518dac8e29935baba8679a
Jakub Delicat <jakub.delicat@husarion.com>
2023-02-07 16:27:35 +0100

    Added timeout to amcl_pose listener

--- healthcheck/healthcheck_localization.py
+++ healthcheck/healthcheck_localization.py
@@ -13,6 +13,6 @@ latching_qos = QoSProfile(depth=1,
 
 node = rclpy.create_node("amcl_pose_listener")
 sub = node.create_subscription(PoseWithCovarianceStamped, "/amcl_pose", callback, qos_profile=latching_qos)
-rclpy.spin_once(node)
+rclpy.spin_once(node, timeout_sec=2.0)
 print(f"amcl_pose timeout")
 exit(1)
